ab180f8553479f79c49c78336dc5aa1b205291ae
Sackbuoy <noremackientz@gmail.com>
2022-02-11 23:42:34 -0600

    formatting and gitignore

--- main.go
+++ main.go
@@ -1,119 +1,122 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
-	"github.com/likexian/whois"
-	"github.com/likexian/whois-parser"
-	"encoding/json"
+  "fmt"
+  "log"
+  "net/http"
+  "github.com/gorilla/mux"
+  "github.com/likexian/whois"
+  "github.com/likexian/whois-parser"
+  "encoding/json"
 )
 
 const PORT = ":8000"
 
 type Response struct {
-	Status 					[]string `json:"status"`
-	CreatedDate			string	 `json:"created`
-	ExpirationDate	string	 `json:"expiry"`
-	Registrar				string	 `json:"registrar"`
-	Registrant			string	 `json:"registrant"`
-	ContactEmail	  string	 `json:"contactEmail"`
+  Status           []string `json:"status"`
+  CreatedDate      string   `json:"created`
+  ExpirationDate  string   `json:"expiry"`
+  Registrar        string   `json:"registrar"`
+  Registrant      string   `json:"registrant"`
+  ContactEmail    string   `json:"contactEmail"`
 }
 
 type ErrorResponse struct {
-	Kind		string `json:"kind"`
-	Code		int    `json:"code"`
-	Message string `json:"message"`
+  Kind    string `json:"kind"`
+  Code    int    `json:"code"`
+  Message string `json:"message"`
 }
 
 const getDomainInfoPath = "/domaininfo/{domain}"
 func getDomainInfo(writer http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	domain := string(vars["domain"])
+  vars := mux.Vars(req)
+  domain := string(vars["domain"])
 
-	writer.Header().Set("Content-Type", "application/json")
+  writer.Header().Set("Content-Type", "application/json")
 
-	whoisResult, whoisErr := whois.Whois(domain)
-	// the whoisparser only supports whois responses for domain names, not IP addresses.
-	// If i have time I will implement a second response type for IP addresses as a workaround
-	parsedResult, parseErr := whoisparser.Parse(whoisResult) 
+  whoisResult, whoisErr := whois.Whois(domain)
+  // the whoisparser only supports whois responses for domain names, not IP addresses.
+  // If i have time I will implement a second response type for IP addresses as a workaround
+  parsedResult, parseErr := whoisparser.Parse(whoisResult) 
 
-	if whoisErr != nil {
-		errorMessage := ""
-		errorCode := 0
-		if whoisErr == whois.ErrDomainEmpty {
-			writer.WriteHeader(http.StatusBadRequest) 
-			errorMessage = "Domain cannot be empty"
-			errorCode = 400
-		} else {
-			writer.WriteHeader(http.StatusInternalServerError) 
-			errorMessage = fmt.Sprintf("Failed to fetch WhoisData for %s", domain)
-			errorCode = 500
-		}
+  if whoisErr != nil {
+    errorMessage := ""
+    errorCode := 0
+    errorType := ""
+    if whoisErr == whois.ErrDomainEmpty {
+      writer.WriteHeader(http.StatusBadRequest) 
+      errorMessage = "Domain cannot be empty"
+      errorCode = 400
+      errorType = "bad_request"
+    } else {
+      writer.WriteHeader(http.StatusInternalServerError) 
+      errorMessage = fmt.Sprintf("Failed to fetch WhoisData for %s", domain)
+      errorCode = 500
+      errorType = "internal_server_error"
+    }
 
-		log.Println(errorMessage)
+    log.Println(errorMessage)
 
-		errResponse := ErrorResponse {
-			Kind:			"internal_server_error",
-			Code:			errorCode,
-			Message: 	whoisErr.Error(),
-		}
+    errResponse := ErrorResponse {
+      Kind:      errorType,
+      Code:      errorCode,
+      Message:   whoisErr.Error(),
+    }
 
-		jsonResponse, err := json.Marshal(errResponse)
-		if err != nil {
-			log.Println("Failed to format WHOIS fetch error as JSON")
-			writer.Write([]byte("500 - Internal Server Error"))
-		}
+    jsonResponse, err := json.Marshal(errResponse)
+    if err != nil {
+      log.Println("Failed to format WHOIS fetch error as JSON")
+      writer.Write([]byte("500 - Internal Server Error"))
+    }
 
-		writer.Write(jsonResponse)
-	} else if parseErr != nil {
-			writer.WriteHeader(http.StatusBadRequest) // 400 Bad request
-			errorMessage := fmt.Sprintf("Failed to parse WHOIS response for domain %s", domain)
+    writer.Write(jsonResponse)
+  } else if parseErr != nil {
+      writer.WriteHeader(http.StatusBadRequest) // 400 Bad request
+      errorMessage := fmt.Sprintf("Failed to parse WHOIS response for domain %s", domain)
 
-			log.Println(errorMessage)
+      log.Println(errorMessage)
 
-			errResponse := ErrorResponse {
-				Kind:			"bad_request",
-				Code:			400,
-				Message:	parseErr.Error(),
-			}
+      errResponse := ErrorResponse {
+        Kind:      "bad_request",
+        Code:      400,
+        Message:  parseErr.Error(),
+      }
 
-			jsonResponse, err := json.Marshal(errResponse)
-			if err != nil {
-				writer.WriteHeader(http.StatusInternalServerError)
-				log.Println("Failed to format parse error as JSON")
-				writer.Write([]byte("500 - Internal Server Error"))
-			}
+      jsonResponse, err := json.Marshal(errResponse)
+      if err != nil {
+        writer.WriteHeader(http.StatusInternalServerError)
+        log.Println("Failed to format parse error as JSON")
+        writer.Write([]byte("500 - Internal Server Error"))
+      }
 
-			writer.Write(jsonResponse)
-	} else {
+      writer.Write(jsonResponse)
+  } else {
 
-			response := Response {
-				Status: 				parsedResult.Domain.Status,
-				CreatedDate: 		parsedResult.Domain.CreatedDate,
-				ExpirationDate: parsedResult.Domain.ExpirationDate,
-				Registrar: 			parsedResult.Registrar.Name,
-				Registrant:			parsedResult.Registrant.Name,
-				ContactEmail: 	parsedResult.Registrant.Email,
-			}
-			
-			writer.WriteHeader(http.StatusOK)
-			jsonResponse, err := json.Marshal(response)
-			if err != nil {
-				writer.WriteHeader(http.StatusInternalServerError)
-				log.Println("Failed to WHOIS response object as JSON")
-				writer.Write([]byte("500 - Internal Server Error"))
-			}
+      response := Response {
+        Status:         parsedResult.Domain.Status,
+        CreatedDate:     parsedResult.Domain.CreatedDate,
+        ExpirationDate: parsedResult.Domain.ExpirationDate,
+        Registrar:       parsedResult.Registrar.Name,
+        Registrant:      parsedResult.Registrant.Name,
+        ContactEmail:   parsedResult.Registrant.Email,
+      }
+      
+      writer.WriteHeader(http.StatusOK)
+      jsonResponse, err := json.Marshal(response)
+      if err != nil {
+        writer.WriteHeader(http.StatusInternalServerError)
+        log.Println("Failed to WHOIS response object as JSON")
+        writer.Write([]byte("500 - Internal Server Error"))
+      }
 
-			writer.Write(jsonResponse)
-		}
+      writer.Write(jsonResponse)
+    }
 }
 
 func main() {
-	log.Println("Server Started")
-	router := mux.NewRouter()
-	router.HandleFunc(getDomainInfoPath, getDomainInfo).Methods("GET")
+  log.Println("Server Started")
+  router := mux.NewRouter()
+  router.HandleFunc(getDomainInfoPath, getDomainInfo).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(PORT, router))
+  log.Fatal(http.ListenAndServe(PORT, router))
 }
