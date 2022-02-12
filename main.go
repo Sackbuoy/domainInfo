1ee0ca1b24b594d11dc51bc96c04f0903bff2a62
Sackbuoy <noremackientz@gmail.com>
2022-02-11 23:36:59 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/likexian/whois"
+	"github.com/likexian/whois-parser"
+	"encoding/json"
+)
+
+const PORT = ":8000"
+
+type Response struct {
+	Status 					[]string `json:"status"`
+	CreatedDate			string	 `json:"created`
+	ExpirationDate	string	 `json:"expiry"`
+	Registrar				string	 `json:"registrar"`
+	Registrant			string	 `json:"registrant"`
+	ContactEmail	  string	 `json:"contactEmail"`
+}
+
+type ErrorResponse struct {
+	Kind		string `json:"kind"`
+	Code		int    `json:"code"`
+	Message string `json:"message"`
+}
+
+const getDomainInfoPath = "/domaininfo/{domain}"
+func getDomainInfo(writer http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	domain := string(vars["domain"])
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	whoisResult, whoisErr := whois.Whois(domain)
+	// the whoisparser only supports whois responses for domain names, not IP addresses.
+	// If i have time I will implement a second response type for IP addresses as a workaround
+	parsedResult, parseErr := whoisparser.Parse(whoisResult) 
+
+	if whoisErr != nil {
+		errorMessage := ""
+		errorCode := 0
+		if whoisErr == whois.ErrDomainEmpty {
+			writer.WriteHeader(http.StatusBadRequest) 
+			errorMessage = "Domain cannot be empty"
+			errorCode = 400
+		} else {
+			writer.WriteHeader(http.StatusInternalServerError) 
+			errorMessage = fmt.Sprintf("Failed to fetch WhoisData for %s", domain)
+			errorCode = 500
+		}
+
+		log.Println(errorMessage)
+
+		errResponse := ErrorResponse {
+			Kind:			"internal_server_error",
+			Code:			errorCode,
+			Message: 	whoisErr.Error(),
+		}
+
+		jsonResponse, err := json.Marshal(errResponse)
+		if err != nil {
+			log.Println("Failed to format WHOIS fetch error as JSON")
+			writer.Write([]byte("500 - Internal Server Error"))
+		}
+
+		writer.Write(jsonResponse)
+	} else if parseErr != nil {
+			writer.WriteHeader(http.StatusBadRequest) // 400 Bad request
+			errorMessage := fmt.Sprintf("Failed to parse WHOIS response for domain %s", domain)
+
+			log.Println(errorMessage)
+
+			errResponse := ErrorResponse {
+				Kind:			"bad_request",
+				Code:			400,
+				Message:	parseErr.Error(),
+			}
+
+			jsonResponse, err := json.Marshal(errResponse)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				log.Println("Failed to format parse error as JSON")
+				writer.Write([]byte("500 - Internal Server Error"))
+			}
+
+			writer.Write(jsonResponse)
+	} else {
+
+			response := Response {
+				Status: 				parsedResult.Domain.Status,
+				CreatedDate: 		parsedResult.Domain.CreatedDate,
+				ExpirationDate: parsedResult.Domain.ExpirationDate,
+				Registrar: 			parsedResult.Registrar.Name,
+				Registrant:			parsedResult.Registrant.Name,
+				ContactEmail: 	parsedResult.Registrant.Email,
+			}
+			
+			writer.WriteHeader(http.StatusOK)
+			jsonResponse, err := json.Marshal(response)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				log.Println("Failed to WHOIS response object as JSON")
+				writer.Write([]byte("500 - Internal Server Error"))
+			}
+
+			writer.Write(jsonResponse)
+		}
+}
+
+func main() {
+	log.Println("Server Started")
+	router := mux.NewRouter()
+	router.HandleFunc(getDomainInfoPath, getDomainInfo).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(PORT, router))
+}
---
+++ main_test.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/gorilla/mux"
+	"log"
+	"bytes"
+	"os"
+	"strings"
+)
+
+const baseURL = "http://0.0.0.0/domaininfo"
+
+
+// Happy path with a valid domain, and no error logs
+func TestGetDomainInfo(t *testing.T) {
+	domain := "whoiswrapper.com"
+
+	// this is for suppressing log output and saving it to check later
+	var logBuf bytes.Buffer
+	log.SetOutput(&logBuf)
+	defer func() {
+      log.SetOutput(os.Stderr)
+  }()
+
+	response := sendRequest(domain, t)
+	logOutput := logBuf.String()
+
+	expected := 200
+	checkStatusCode(response.Code, expected, t)
+
+	if !(len(logOutput) == 0) {
+		t.Fatal(fmt.Sprintf("Expected no log output but got %s", logOutput))
+	}
+	
+	if response.Body == nil {
+		t.Fatal("Got no response body from WHOIS call")
+	}
+}
+
+
+// Templated tests for nonexistent, malformatted, or empty/blank domain names
+func TestBadDomain(t *testing.T) {
+	var tests = []struct {
+		domain string
+	}{
+		{"bad_domain"},
+		{"!@#$"},
+		{" "},
+		{""},
+	}
+
+	for _, testTable := range tests {
+		testName := fmt.Sprintf("%s", testTable.domain)
+		t.Run(testName, func(t *testing.T) {
+
+			// this is for suppressing log output and saving it to check later
+			var logBuf bytes.Buffer
+			log.SetOutput(&logBuf)
+			defer func() {
+		      log.SetOutput(os.Stderr)
+		  }()
+			
+			response := sendRequest(testTable.domain, t)
+			logOutput := logBuf.String()
+
+			expected := 400
+			checkStatusCode(response.Code, expected, t)
+
+			expectedMessage := findExpectedMessage(testTable.domain)
+			checkLogOutput(expectedMessage, logOutput, t)
+
+			if response.Body == nil {
+				t.Fatal("Got no response body from WHOIS call")
+			}
+		})
+	}
+}
+
+func findExpectedMessage(domain string) (message string) {
+	if strings.TrimSpace(domain) == "" {
+		return "Domain cannot be empty"
+	} else {
+		return fmt.Sprintf("Failed to parse WHOIS response for domain %s", domain)
+	}
+}
+
+func checkLogOutput(expected string, actual string, t *testing.T) {
+	if !strings.Contains(actual, expected) {
+		t.Fatal(fmt.Sprintf("Expected: \n %s to contain: \n %s", actual, expected))
+	}
+}
+
+func sendRequest(domain string, t *testing.T) *httptest.ResponseRecorder {
+	req, err := http.NewRequest("GET", baseURL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	vars := map[string]string{
+		"domain": domain,
+	}
+	req = mux.SetURLVars(req, vars)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(getDomainInfo)
+	handler.ServeHTTP(responseRecorder, req)
+	return responseRecorder
+}
+
+func checkStatusCode(code int, expected int, t *testing.T) {
+	if code != expected {
+		errorMsg := fmt.Sprintf("Expected status code %d but got %d", expected, code)
+		t.Fatal(errorMsg)
+	}
+}
