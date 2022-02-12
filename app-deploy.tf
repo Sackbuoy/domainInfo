8b47ba0cf2118e6779ee3b19998051abf26b84de
Sackbuoy <noremackientz@gmail.com>
2022-02-12 14:02:17 -0600

    changing from git to github for clone and redeploying

--- app-deploy.tf
+++ app-deploy.tf
@@ -2,18 +2,16 @@ resource "digitalocean_app" "domaininfo" {
   spec {
     name   = "domaininfo"
     region = "nyc3"
-    domain {
-      name = "whoiswrapper.com"
-    }
 
     service {
       name                  = "domaininfo-service"
       build_command         = "go test && go build ."
       run_command           = "./domainInfo"
 
-      git {
-        repo_clone_url = "https://github.com/Sackbuoy/domainInfo"
+      github {
+        repo           = "Sackbuoy/domainInfo"
         branch         = "main"
+        deploy_on_push = true
       }
     }
   }
